39983781360a59b4ab233a237427bae713526e85
iffat <iffi9955@gmail.com>
2024-07-28 00:47:27 +0500

    adding

---
+++ utility/sendTGNotification.js
@@ -0,0 +1,24 @@
+let botToken = "7370590624:AAHO_mv2jTRPw7YlGXcIOs_X6CSb1vEzlrI"
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(botToken, { polling: true });
+const channelId = "-1002178471951"
+const sendAlert = async(message) => {
+    try{
+        bot.sendMessage(channelId, message)
+        .then(() => {
+            console.log('Notification sent successfully');
+            return true;
+        })
+        .catch((error) => {
+            console.error('Error sending notification:', error);
+            return false;
+        });
+    }catch(error){
+        console.log("error ===>>>>", error)
+        return false
+    }
+}
+
+module.exports = {
+    sendAlert
+}
