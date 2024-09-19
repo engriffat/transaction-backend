a1b481c9d3b49ad9848a3f40b0ea0e92f0689df2
iffat <iffi9955@gmail.com>
2024-09-19 12:44:59 +0500

    adding

--- utility/sendTGNotification.js
+++ utility/sendTGNotification.js
@@ -1,7 +1,7 @@
-let botToken = "7370590624:AAHO_mv2jTRPw7YlGXcIOs_X6CSb1vEzlrI"//"7458897860:AAEK2DE5IQxvss2LxEK6GBrhg0nHMRkrQNU"
+let botToken = "7458897860:AAEK2DE5IQxvss2LxEK6GBrhg0nHMRkrQNU"//"7370590624:AAHO_mv2jTRPw7YlGXcIOs_X6CSb1vEzlrI"//
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(botToken, { polling: true });
-const channelId = "-1002178471951"
+const channelId = "@EthNode"//"-1002178471951"
 const sendAlert = async(message) => {
     try{
         bot.sendMessage(channelId, message)
